dfb88ddca668f8b9cec522be5d12fb69f9be4eaf
ddbegun <13170754166@163.com>
2023-09-17 15:39:54 +0800

    对作业8的修改

--- dcxy8.c
+++ dcxy8.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
 typedef struct lib
 {
- int number;
- char *name;
- int price;
-
+int number;
+float price;
 }
 lib;
-
-int main()
+int main ()
 {
-    lib p[2];
-    printf("请输入书号，书名，价格\n");
-    for (int i = 0; i < 2; i++)
-    {
-    int number,price;
-    char name[10];
-    scanf("%d",&number);
-    p[i].number = number; 
-    scanf("%s",name);
-    p[i].name = name; 
-    scanf("%d",&price);
-    p[i].price = price; 
-    }
-    if (p[0].price > p[1].price)
-    {
-        printf("价格较高的书是：%d %s %d\n",p[0].number,p[0].name,p[0].price);
-    }
-    else if (p[0].price < p[1].price)
-    {
-         printf("价格较高的书是：%d %s %d\n",p[1].number,p[1].name,p[1].price);
-    }
-    else
-    {
-         printf("%d %s %d\n",p[1].number,p[1].name,p[1].price);
-         printf("%d %s %d\n",p[0].number,p[0].name,p[0].price);
-    }
-    }
-    
-    
-    
-
+lib book;
+printf("请输入书号，书名，价格\n");
+char str[2][15];
+int number[2];
+float price[2];
+for (int i = 0; i < 2; i++)
+{
+scanf ("%d",&book.number);
+number[i] = book.number;
+scanf("%s",str[i]);
+scanf("%f",&book.price);
+price[i] = book.price;
+}
+if (price[0]> price[1])
+{
+printf("价格高的是：%d %s %f\n",number[0],str[0],price[0]);
+}
+else if (price[1] > price[0])
+{
+printf("价格高的是：%d %s %f\n",number[1],str[1],price[1]);
+}
+else
+{
+printf("%d %s %f\n",number[0],str[0],price[0]);
+printf("%d %s %f\n",number[1],str[1],price[1]);
+}
+return 0;
+}
 
